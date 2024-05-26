219088e6d361c118c60bf4f196c0041a11f7a519
kelvys B. Pantaleão <contato@kelvysb.com>
2024-05-26 12:57:36 -0300

    Adicionado componente de salvamento

---
+++ projeto/addons/Salvamento/plugin.gd
@@ -0,0 +1,10 @@
+tool
+extends EditorPlugin
+
+const AUTOLOAD_NAME = "Salvamento"
+
+func _enter_tree():	
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/Salvamento/Salvamento.gd")
+
+func _exit_tree():
+	remove_autoload_singleton(AUTOLOAD_NAME)
