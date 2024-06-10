feacb85a1dbd247606b0a9cfb693a2b427894c5e
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2024-06-10 18:57:58 -0500

    refactor & type hints

--- projeto/addons/controle_de_toque/controle_toque.gd
+++ projeto/addons/controle_de_toque/controle_toque.gd
@@ -1,50 +1,32 @@
 extends Node
 
-signal coordenadas_de_toque_y #(coordenadas y)
-signal coordenadas_de_toque_x #(coordenadas x)
-signal toque_realizado #toque realizado
-signal toque_desfeito #toque desfeito
+signal toque_realizado(historico) #toque realizado
+signal toque_desfeito(historico) #toque desfeito
+signal arrastar_realizado(historico) #tela arrastada
 
 export var tamanho_array = 2 #Tamanho total que a array pode chegar
 
 onready var viewport_modificador = get_viewport().get_visible_rect().size * .5 #Pega o viewport e a area visivel dele
 
-var posicao_atual #Posição atual aplicada no viewport
-var historico_de_toque_x = [] #Array do eixo x
-var historico_de_toque_y = [] #Array do eixo y
-var foi_toque_realizado = false #Booleana para confirmar toque realizado
+var posicao_atual: Vector2 #Posição atual aplicada no viewport
+var historico: PoolVector2Array = []
 
 func _input(event):
 	if event is InputEventScreenDrag: #Se o evento for um drag de input na tela
 		posicao_atual = (event.get_position() - viewport_modificador) #Posição atual é igual a posição do evento - o tamanho total da tela
 
-		if event.is_pressed():
-			_atualizar_historico()
-			emit_signal("toque_realizado", foi_toque_realizado)
-
-		else:
-			_atualizar_historico()
-			emit_signal("toque_desfeito", foi_toque_realizado)
+		_atualizar_historico()
+		emit_signal("arrastar_realizado", historico)
 
 	if event is InputEventScreenTouch:
 		if event.is_pressed():
-			foi_toque_realizado = true
-			emit_signal("toque_realizado", foi_toque_realizado)
+			emit_signal("toque_realizado", historico)
 
 		else:
-			foi_toque_realizado = false
-			emit_signal("toque_desfeito", foi_toque_realizado)
+			emit_signal("toque_desfeito", historico)
 
 func _atualizar_historico():
-	historico_de_toque_y.append(posicao_atual.y)
-	historico_de_toque_x.append(posicao_atual.x)
+	historico.append(posicao_atual)
+	if len(historico) > tamanho_array:
+		historico.remove(0)
 
-	if len(historico_de_toque_x) > tamanho_array:
-		historico_de_toque_x.pop_front()
-	if len(historico_de_toque_y) > tamanho_array:
-		historico_de_toque_y.pop_front()
-
-	emit_signal("coordenadas_de_toque_x", historico_de_toque_x)
-	emit_signal("coordenadas_de_toque_y", historico_de_toque_y)
-
-#	$Label.text = "x=" + String(historico_de_toque_x) + " y=" + String(historico_de_toque_y)
