225f596bce6afee7647ff1a9553e2a6f6f02d963
Mihailov Vasilievic Filho <mihailov-vf@users.noreply.github.com>
2024-06-15 17:57:04 +0000

    Revert "Checagem de colisão para dano do jogador"
    This reverts commit 8c80245548b0d70ab608f2eab01d40b27a6950da.

--- projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
+++ projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
@@ -2,9 +2,4 @@ extends KinematicBody
 class_name PlayerLane3D
 
 onready var controle_faixa_3d = $ControleFaixa3D
-onready var vida = $Vida as Vida
-
-
-func _on_AreaDano_body_entered(body: Node) -> void:
-	if(body.is_in_group('obstaculo')):
-		vida.receber_dano(1.0)
+onready var vida = $Vida
