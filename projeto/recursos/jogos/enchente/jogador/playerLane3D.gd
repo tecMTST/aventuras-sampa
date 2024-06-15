c4570c394cec258c86049919a6b3588df0010f5a
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2024-06-15 15:16:48 -0500

    adicionando vida

--- projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
+++ projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
@@ -2,6 +2,7 @@ extends KinematicBody
 class_name PlayerLane3D
 
 onready var controle_faixa_3d = $ControleFaixa3D
+onready var vida = $Vida as Vida
 
 
 func _on_ControladorArrasta_arrastado(chave):
