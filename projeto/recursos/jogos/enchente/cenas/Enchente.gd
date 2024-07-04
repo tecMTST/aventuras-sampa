764d010fcdc5ad5e0f81f117cb7e0fcc924e57f5
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-07-03 22:28:45 -0300

    Separação de responsabilidades

--- projeto/recursos/jogos/enchente/cenas/Enchente.gd
+++ projeto/recursos/jogos/enchente/cenas/Enchente.gd
@@ -1,23 +1,26 @@
 extends Spatial
 
+# Tempo de jogo em minutos
+export var tempo_jogo = 5
+
 onready var player_lane_3d = $playerLane3D as PlayerLane3D
 onready var faixa_1 = $Faixas/Faixa1
 onready var faixa_2 = $Faixas/Faixa2
 onready var faixa_3 = $Faixas/Faixa3
-onready var origem_obstaculos = $Faixas/OrigemObstaculos
 onready var fps_label = $CanvasLayer/Control/HBoxContainer/FpsLabel
+onready var agua = $Agua
 
+onready var _tempo = $TempoDeJogo
 
 func _ready():
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_1.global_position)
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_2.global_position)
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_3.global_position)
 	player_lane_3d.controle_faixa_3d.posicao_inicial = 1
+	_tempo.wait_time = tempo_jogo * 60
+	_tempo.start()
+
 
 func _process(_delta):
 	fps_label.text = str(Engine.get_frames_per_second())
-
-
-func _on_AreaColetoraObstaculos_body_entered(body):
-	if((body as Node).is_in_group("obstaculo")):
-		(body as Node).queue_free()
+	agua.get_active_material(0).set_shader_param("Velocidade",EnchenteEstadoDeJogo.VelocidadeGlobal);
--- projeto/recursos/jogos/enchente/componentes/Obstaculo.gd
+++ projeto/recursos/jogos/enchente/componentes/Obstaculo.gd
@@ -3,5 +3,10 @@ extends KinematicBody
 
 export var velocidade = 1000.0
 
+
 func _process(delta):
 	move_and_slide(Vector3(0, 0, velocidade * EnchenteEstadoDeJogo.VelocidadeGlobal * delta), Vector3.UP)
+
+
+func _on_VisibilityNotifier_screen_exited() -> void:
+	queue_free()
