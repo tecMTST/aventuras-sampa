14bcba095f4ecb2153d9603fad2128de2b665d42
KelvysB <contato@kelvysb.com>
2024-07-07 19:40:43 -0300

    Adiocionado o efeito de imunidade

--- projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
+++ projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
@@ -1,9 +1,16 @@
 extends KinematicBody
 class_name PlayerLane3D
 
+export var tempo_imunidade_dano : float = 3.0
 onready var controle_faixa_3d = $ControleFaixa3D
 onready var vida = $Vida as Vida
+onready var sprite = $Sprite3D
+onready var timer_imunidade = $TimerImunidade
+onready var imunidade_modulate = false
 
+var imune = false
+var imune_dano = false
+var imunidade_time : float = 0
 
 func _on_ControladorArrasta_arrastado(chave):
 	if chave=='direita':
@@ -14,9 +21,33 @@ func _on_ControladorArrasta_arrastado(chave):
 
 
 func _on_AreaDano_body_entered(body: Node) -> void:
-	if body.is_in_group("obstaculo"):
+	if body.is_in_group("obstaculo") and not imune:
 		vida.receber_dano(1.0)
-
+		imunidade(true, tempo_imunidade_dano)
 
 func _on_Vida_vida_acabou() -> void:
 	TrocadorDeCenas.trocar_cena('res://recursos/feed_de_noticias/feed_de_noticia.tscn')
+	
+func imunidade(dano : bool, tempo : float):
+	imunidade_time = tempo
+	imune_dano = dano
+	imune = true
+	timer_imunidade.start()
+	
+func _on_TimerImunidade_timeout():
+	if imune_dano:
+		sprite.visible = not sprite.visible
+	else:
+		imunidade_modulate = not imunidade_modulate
+		if imunidade_modulate:			
+			sprite.modulate = Color (1.5, 1.5, 1)	
+		else: 
+			sprite.modulate = Color (1, 1, 1)	
+	imunidade_time = imunidade_time - timer_imunidade.wait_time
+	if imunidade_time <= 0:
+		sprite.visible = true
+		imune = false
+		imune_dano = false
+		imunidade_modulate = false
+		sprite.modulate = Color (1, 1, 1)	
+		timer_imunidade.stop()
