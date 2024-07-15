285a6d7140e849e2c4cf15b62ced0a2f6197572e
hiroto <rmoraisu@protonmail.com>
2024-07-15 14:18:54 -0500

    temporizador e limite de seguidores

--- projeto/recursos/jogo/jogador/jogador.gd
+++ projeto/recursos/jogo/jogador/jogador.gd
@@ -5,19 +5,45 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 export (int) var multiplicador_velocidade = 200
+export (int) var maximo_seguidores = 20
+export (int) var tempo_segundos = 180
+export var loc_temporizador: NodePath
 
 onready var sprite = $SpritesJogador as AnimatedSprite
+onready var barra_seguidores = $BarraSeguidores as ProgressBar
+onready var barra_tempo = $TempoRestante as ProgressBar
+onready var temporizador = get_node(loc_temporizador) as Timer
 
+var aleatorio = RandomNumberGenerator.new()
+var _seguidores = []
 var velocidade = Vector2()
 var _movendo = false
 var jogador = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rotation_degrees = 0
+	aleatorio.randomize()
+	
+	barra_seguidores.max_value = maximo_seguidores
+	
+	temporizador.wait_time = tempo_segundos
+	temporizador.autostart = false
+	barra_tempo.max_value = tempo_segundos
+	barra_tempo.value = temporizador.wait_time
+	temporizador.start()
 
 func toque():
 	pass
+
+func adicionar_seguidor(seguidor):
+	if _seguidores.size() >= maximo_seguidores:
+		return false
+	_seguidores.append(seguidor)
+	return true
+
+func remover_seguidor(seguidor):
+	if seguidor in _seguidores:
+		_seguidores.remove(_seguidores.find(seguidor))
 
 func get_input():
 	velocidade = Vector2()
@@ -47,7 +73,8 @@ func _process(delta):
 		sprite.play("andar")
 	else:
 		sprite.play("parar")
-
+	barra_seguidores.value = _seguidores.size()
+	barra_tempo.value = temporizador.time_left
 
 func _physics_process(delta):
 	velocidade = move_and_slide(velocidade)
