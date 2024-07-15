b15394759daae2ad3d27e787fe5bdbecdba7cd37
hiroto <rmoraisu@protonmail.com>
2024-07-15 14:18:34 -0500

    area de seguidores

--- projeto/recursos/jogo/pessoa/pessoa.gd
+++ projeto/recursos/jogo/pessoa/pessoa.gd
@@ -12,18 +12,16 @@ export var variedade_aleatorio: int = 200
 onready var maquina_de_estados = $MaquinaDeEstados as MaquinaDeEstados
 onready var sprites = $SpritesPessoa as AnimatedSprite
 
-var aleatorio = RandomNumberGenerator.new()
 var _alvo: KinematicBody2D
 var distancia_maxima: int
 var cor='azul'
+var aleatorio: RandomNumberGenerator
 
 onready var vetor_diferente: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	aleatorio.randomize()
-	distancia_maxima = aleatorio.randi_range(variedade_distancia_maxima.x,variedade_distancia_maxima.y)
-	vetor_diferente = gerar_vetor_aleatorio()
+	pass
 
 func gerar_vetor_aleatorio():
 	return Vector2(
@@ -31,6 +29,10 @@ func gerar_vetor_aleatorio():
 		aleatorio.randf_range(-variedade_aleatorio, variedade_aleatorio))
 
 func definir_alvo(alvo):
+	aleatorio = alvo.aleatorio
+	aleatorio.randomize()
+	distancia_maxima = aleatorio.randi_range(variedade_distancia_maxima.x,variedade_distancia_maxima.y)	
+	vetor_diferente = gerar_vetor_aleatorio()
 	_alvo = alvo
 	cor='vermelha'
 
@@ -61,6 +63,8 @@ func _process(delta):
 
 
 func _on_Area_body_entered(body):
-	if not body.jogador:
+	if not body.jogador or (_alvo):
+		return
+	if not body.adicionar_seguidor(self):
 		return
 	definir_alvo(body)
