52fd3169ead45ebf5c0e7c77d1716cffbb9aa93e
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-07-18 09:58:07 -0300

    Revert "Merge branch '72-modelo-de-tipos-de-obstaculos-no-json'"
    This reverts commit 1dbf7ae20e52b23701b3ff8ad6b245ff0e1c2934, reversing
    changes made to d0c64037830c6ce8e9c4aff12629f8d4ea92c2bb.

--- projeto/recursos/jogos/enchente/barra_de_progresso/BarraDeProgresso.gd
+++ projeto/recursos/jogos/enchente/barra_de_progresso/BarraDeProgresso.gd
@@ -1,19 +1,27 @@
 extends Control
 
+
+export var minutos: float = 5
+
 signal tempo_acabado
 
-export(NodePath) var tempo
-
-onready var _tempo := get_node(tempo) as Timer
+onready var tempo = Timer.new()
 onready var barra = $ProgressoDeTextura
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_tempo.connect("timeout", self, "_ao_tempo_acabar")
+	add_child(tempo)
+	tempo.one_shot = true
+	tempo.wait_time = 60 * minutos
+	tempo.connect("timeout", self, "_ao_tempo_acabar")
+
+
+func iniciar():
+	tempo.start()
 
 
 func _process(delta):
-	barra.value = 2 * barra.rect_size.x * (1 - _tempo.time_left / _tempo.wait_time)
+	barra.value = 2 * barra.rect_size.x * (1 - tempo.time_left / tempo.wait_time)
 
 
 func _ao_tempo_acabar():
--- projeto/recursos/jogos/enchente/cenas/Enchente.gd
+++ projeto/recursos/jogos/enchente/cenas/Enchente.gd
@@ -1,31 +1,43 @@
 extends Spatial
 
-# Tempo de jogo em minutos
-export var tempo_jogo = 5
-
 onready var player_lane_3d = $playerLane3D as PlayerLane3D
 onready var faixa_1 = $Faixas/Faixa1
 onready var faixa_2 = $Faixas/Faixa2
 onready var faixa_3 = $Faixas/Faixa3
+onready var origem_obstaculos = $Faixas/OrigemObstaculos
 onready var fps_label = $CanvasLayer/Control/HBoxContainer/FpsLabel
-onready var tempo_label = $CanvasLayer/Control/HBoxContainer/Tempo
-onready var agua = $Agua
+
 
 func _ready():
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_1.global_position)
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_2.global_position)
 	player_lane_3d.controle_faixa_3d.faixas.append(faixa_3.global_position)
-	player_lane_3d.controle_faixa_3d.posicao_inicial = 1
-
-	EnchenteEstadoDeJogo.TemporizadorGlobal = $TempoDeJogo
-	EnchenteEstadoDeJogo.definir_tempo_de_jogo(tempo_jogo * 60)
-	EnchenteEstadoDeJogo.iniciar_temporizador()
-	EnchenteEstadoDeJogo.set_process(true)
-
+	player_lane_3d.controle_faixa_3d.posicao_inicial = 1	
+	
 func _process(_delta):
 	fps_label.text = str(Engine.get_frames_per_second())
-	tempo_label.text = String(EnchenteEstadoDeJogo.TempoAtual)
-	agua.get_active_material(0).set_shader_param("Velocidade", EnchenteEstadoDeJogo.VelocidadeGlobal);
 
-func _exit_tree() -> void:
-	EnchenteEstadoDeJogo.set_process(false)
+
+func _on_TimerObstaculos_timeout():
+	randomize()
+	var posicao = randi() % 3 + 1
+	var obstaculo = load("res://recursos/jogos/enchente/componentes/Obstaculo.tscn")
+			
+	for faixa in range(3):
+		if posicao - 1 != faixa:
+			if faixa == 0:				
+				var instancia0 = obstaculo.instance() as KinematicBody
+				add_child(instancia0)
+				instancia0.global_position = Vector3(faixa_1.global_position.x, origem_obstaculos.global_position.y, origem_obstaculos.global_position.z)
+			elif faixa == 1:				
+				var instancia1 = obstaculo.instance() as KinematicBody
+				add_child(instancia1)
+				instancia1.global_position = Vector3(faixa_2.global_position.x, origem_obstaculos.global_position.y, origem_obstaculos.global_position.z)
+			else:
+				var instancia2 = obstaculo.instance() as KinematicBody
+				add_child(instancia2)
+				instancia2.global_position = Vector3(faixa_3.global_position.x, origem_obstaculos.global_position.y, origem_obstaculos.global_position.z)
+		
+func _on_AreaColetoraObstaculos_body_entered(body):
+	if((body as Node).is_in_group("obstaculo")):
+		(body as Node).queue_free()
