75642c3cfe559d43598dfcef721667536781caa9
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-07-18 23:08:06 -0300

    Revert "Revert "Merge branch '72-modelo-de-tipos-de-obstaculos-no-json'""
    This reverts commit 52fd3169ead45ebf5c0e7c77d1716cffbb9aa93e.

--- projeto/recursos/jogos/enchente/componentes/Obstaculo.gd
+++ projeto/recursos/jogos/enchente/componentes/Obstaculo.gd
@@ -1,6 +1,14 @@
+class_name Obstaculo
 extends KinematicBody
 
-export var velocidade = 15.0
+export var velocidade := 1000.0
+export var textura: Texture
+
+func _ready() -> void:
+	$Sprite3D.texture = textura
 
 func _process(delta):
-	move_and_slide(Vector3(0, 0, velocidade * EnchenteEstadoDeJogo.VelocidadeGlobal), Vector3.UP)
+	move_and_slide(Vector3(0, 0, velocidade * EnchenteEstadoDeJogo.VelocidadeGlobal * delta), Vector3.UP)
+
+func _on_VisibilityNotifier_screen_exited() -> void:
+	queue_free()
--- projeto/recursos/jogos/enchente/scripts/EnchenteEstadoDeJogo.gd
+++ projeto/recursos/jogos/enchente/scripts/EnchenteEstadoDeJogo.gd
@@ -1,3 +1,14 @@
 extends Node
 
 var VelocidadeGlobal = 1.0
+var TemporizadorGlobal := Timer.new()
+var TempoAtual := 0.0
+
+func _process(_delta: float) -> void:
+	TempoAtual = TemporizadorGlobal.wait_time - TemporizadorGlobal.time_left
+
+func definir_tempo_de_jogo(tempo: float) -> void:
+	TemporizadorGlobal.wait_time = tempo
+
+func iniciar_temporizador() -> void:
+	TemporizadorGlobal.start()
