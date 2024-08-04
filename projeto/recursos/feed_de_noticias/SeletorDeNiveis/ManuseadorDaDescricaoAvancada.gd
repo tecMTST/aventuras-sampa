73d0c5a606577913b90f587159073c9c3e1ee667
ClockWrites_Dev <clockwritestwitter@gmail.com>
2024-08-04 14:58:44 -0300

    mudanças finais

--- projeto/recursos/feed_de_noticias/SeletorDeNiveis/ManuseadorDaDescricaoAvancada.gd
+++ projeto/recursos/feed_de_noticias/SeletorDeNiveis/ManuseadorDaDescricaoAvancada.gd
@@ -1,16 +1,15 @@
-extends Control
+extends CanvasLayer
 
-onready var card_img: TextureRect = $PainelSaibaMais/PainelDescricao/ImgCard
-onready var desc_avanc: Label = $PainelSaibaMais/PainelDescricao/Desc
-onready var btn_jogar = $PainelSaibaMais/PainelDescricao/Jogar
-onready var btn_saiba_menos = $PainelSaibaMais/PainelDescricao/SaibaMenos
-onready var botao_jogar_tween = $PainelSaibaMais/PainelDescricao/Jogar/TweenJogar
-onready var botao_SaibaMenos_tween = $PainelSaibaMais/PainelDescricao/SaibaMenos/TweenSaibaMenos
-onready var TweenFundo = $PainelSaibaMais/Fundo/TweenFundo
-onready var TweenPainel = $PainelSaibaMais/PainelDescricao/TweenPainel
-onready var animationplayer = $PainelSaibaMais/AnimationPlayer
-#onready var Fundo = $PainelSaibaMais/Fundo
-#onready var PainelDescricao = $PainelSaibaMais/PainelDescricao
+onready var card_img: TextureRect = $PainelDescricao/ImgCard
+onready var desc_avanc: Label = $PainelDescricao/Desc
+onready var btn_jogar = $PainelDescricao/Jogar
+onready var btn_saiba_menos = $PainelDescricao/SaibaMenos
+onready var botao_jogar_tween = $PainelDescricao/Jogar/TweenJogar
+onready var botao_SaibaMenos_tween = $PainelDescricao/SaibaMenos/TweenSaibaMenos
+onready var TweenFundo = $Fundo/TweenFundo
+onready var TweenPainel = $PainelDescricao/TweenPainel
+onready var Fundo = $Fundo
+onready var PainelDescricao = $PainelDescricao
 
 var Descricao_avancada: String
 var Imagem_card: StreamTexture
@@ -26,21 +25,14 @@ func _ready():
 
 	card_img.texture = Imagem_card
 	desc_avanc.text = Descricao_avancada
-	animationplayer.play("Subindo")
-
-func _on_TweenJogar_tween_completed(object, key):
-	TrocadorDeCenas.trocar_cena(Cena_do_jogo)
-	self.queue_free()
-
-func _on_Panel_gui_input(event):
-	if event is InputEventScreenTouch:
-		botao_jogar_tween.start()
-		botao_jogar_tween.interpolate_property(btn_jogar, "rect_scale", Vector2(1, 1), Vector2(1.1, 1.1), 0.1, Tween.TRANS_ELASTIC)
-		botao_jogar_tween.interpolate_property(btn_jogar, "rect_scale", Vector2(1.1, 1.1), Vector2(1, 1), 0.1, Tween.TRANS_ELASTIC)
+	
+	TweenPainel.interpolate_property(PainelDescricao, "modulate:a", 0, 1, 0.7, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	TweenFundo.interpolate_property(Fundo, "modulate:a", 0, 1, 0.7, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	TweenPainel.start()
+	TweenFundo.start()
 
 func _auto_deletar():
 	if saiba_menos_apertado:
-		yield(animationplayer, "animation_finished")
 		self.queue_free()
 
 func _on_SaibaMenos_button_up():
@@ -48,10 +40,17 @@ func _on_SaibaMenos_button_up():
 	botao_SaibaMenos_tween.interpolate_property(btn_saiba_menos, "rect_scale", Vector2(1, 1), Vector2(1.1, 1.1), 0.1, Tween.TRANS_ELASTIC)
 	botao_SaibaMenos_tween.interpolate_property(btn_saiba_menos, "rect_scale", Vector2(1.1, 1.1), Vector2(1, 1), 0.1, Tween.TRANS_ELASTIC)
 	saiba_menos_apertado = true
-	animationplayer.play_backwards("Subindo")
+	TweenPainel.interpolate_property(PainelDescricao, "modulate:a", 1, 0, 0.7, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	TweenFundo.interpolate_property(Fundo, "modulate:a", 1, 0, 0.7, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	TweenPainel.start()
+	TweenFundo.start()
+	yield(TweenFundo, "tween_completed")
 	_auto_deletar()
-#	TweenPainel.interpolate_property(PainelDescricao, "rect_position", Vector2(360, 277), Vector2(360, 1470), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-#	TweenPainel.start()
-#	yield(TweenPainel, "tween_completed")
-#	TweenFundo.interpolate_property(Fundo, "rect_position", Vector2(360, 3), Vector2(360, 1326), 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
-#	TweenFundo.start()
+
+func _on_Jogar_button_up():
+	botao_jogar_tween.start()
+	botao_jogar_tween.interpolate_property(btn_jogar, "rect_scale", Vector2(1, 1), Vector2(1.1, 1.1), 0.1, Tween.TRANS_ELASTIC)
+	botao_jogar_tween.interpolate_property(btn_jogar, "rect_scale", Vector2(1.1, 1.1), Vector2(1, 1), 0.1, Tween.TRANS_ELASTIC)
+	yield(botao_jogar_tween, "tween_completed")
+	TrocadorDeCenas.trocar_cena(Cena_do_jogo)
+	self.queue_free()
