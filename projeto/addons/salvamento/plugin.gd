97bca7c09503903360aeb620e3bfb455ccbacfb6
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-08-11 18:52:35 -0300

    Correção de caminho do plugin

--- projeto/addons/salvamento/plugin.gd
+++ projeto/addons/salvamento/plugin.gd
@@ -4,7 +4,7 @@ extends EditorPlugin
 const AUTOLOAD_NAME = "Salvamento"
 
 func _enter_tree():	
-	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/Salvamento/Salvamento.gd")
+	add_autoload_singleton(AUTOLOAD_NAME, "res://addons/salvamento/Salvamento.gd")
 
 func _exit_tree():
 	remove_autoload_singleton(AUTOLOAD_NAME)
