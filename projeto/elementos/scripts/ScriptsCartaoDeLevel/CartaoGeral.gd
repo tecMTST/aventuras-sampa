38d42bf3f201b11599215fce33b79b655d030f29
ClockWrites_Dev <clockwritestwitter@gmail.com>
2024-08-18 23:01:50 -0300

    novo menu finalizado

--- projeto/elementos/scripts/ScriptsCartaoDeLevel/CartaoGeral.gd
+++ projeto/elementos/scripts/ScriptsCartaoDeLevel/CartaoGeral.gd
@@ -6,6 +6,8 @@ export var idCard = "Teste"
 export var Imagem_card: StreamTexture
 export var E_jogavel: bool
 export var Cena_do_jogo: String
+export var img_coracao_nao_apertado: StreamTexture
+export var img_coracao_apertado: StreamTexture 
 
 onready var card_img: TextureRect = $PainelPrincipal/ImgCard
 onready var card_2: TextureRect = $PainelHidden/outline3/ImgCard2
@@ -13,6 +15,7 @@ onready var titulo_texto: RichTextLabel = $PainelPrincipal/Titulo
 onready var desc_basica: Label = $PainelPrincipal/Desc
 onready var animacao_coracao: AnimationPlayer = $AnimationCoracao
 onready var botao_saibaMais = $PainelPrincipal/SaibaMais
+onready var tween_card: Tween = $Tween
 
 var saiba_mais_apertado: bool = false
 var coracao_apertado: bool = false
@@ -53,25 +56,36 @@ func _atualizar_painel():
 func _ready():
 	recarregar_like()
 	_atualizar_card()
+	var save = Salvamento.carregar_tudo("like")
+	var dados = save.Dados
+	print(dados[idCard])
+	print(dados)
 
 func recarregar_like():
 	if not Salvamento.existe("like", idCard):
-		print('Não possue like')
+	#	print('Não possue like')
 		save_like()
 	else:
 		var save = Salvamento.carregar_tudo("like")
 		var dados = save.Dados
-		print(dados)
+		if dados[idCard] == false:
+			$PainelPrincipal/Coracao.texture = img_coracao_nao_apertado
+			coracao_apertado = false
+		else:
+			$PainelPrincipal/Coracao.texture = img_coracao_apertado
+			coracao_apertado = true
 
 func _tocar_animacao_coracao():
 	if coracao_apertado == false:
 		animacao_coracao.play("Animação")
 		yield(animacao_coracao, "animation_finished")
 		coracao_apertado = true
+		save_like()
 	else:
 		animacao_coracao.play_backwards("Animação")
 		yield(animacao_coracao, "animation_finished")
 		coracao_apertado = false
+		save_like()
 
 func _on_SaibaMais_pressed():
 	var botao_tween = $Tween
@@ -107,3 +121,10 @@ func loadJson(nomejson):
 	else:
 		print("File Open Error: could not open file " + nomejson)
 	arquivo.close()
+
+func _on_VisibilityNotifier2D_screen_entered():
+	tween_card.interpolate_property($PainelPrincipal, "rect_position", Vector2(55, -5), Vector2(55, 32), 0.7, Tween.TRANS_LINEAR)
+	tween_card.interpolate_property($PainelPrincipal, "modulate:a", 0.2, 1, 0.5, Tween.TRANS_LINEAR)
+	tween_card.interpolate_property($PainelHidden, "rect_position", Vector2(19, -5), Vector2(19, 60), 0.7, Tween.TRANS_LINEAR)
+	tween_card.interpolate_property($PainelHidden, "modulate:a", 0.2, 1, 0.5, Tween.TRANS_LINEAR)
+	tween_card.start()
