d8895d90f637c0f804378fb89ab1bdac94e87ce6
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-09-01 17:52:59 -0300

    Quantidade de nuvens e cidade subindo

--- projeto/elementos/scripts/ScriptsBackgroundCidadeSubindo/cidadeSubindo.gd
+++ projeto/elementos/scripts/ScriptsBackgroundCidadeSubindo/cidadeSubindo.gd
@@ -3,6 +3,7 @@ extends Spatial
 signal animacaoTerminou
 
 onready var timerDoTempoDeEspera: Timer = $TimerDoTempoDeEspera
+onready var tween := $Tween
 
 export var tempoDeEsperaPrimeiraFase: int = 25 #em segundos
 export var tempoDeEsperaSegundaFase: int = 25 #em segundos
@@ -11,4 +12,11 @@ var faseAtual: int = 0
 var ativadoPorSinal: bool = false
 
 func _ready() -> void:
-	pass
+	timerDoTempoDeEspera.start()
+
+func _mover_cidade() -> void:
+	var posicao_inicial := Vector3(0,-2.5, 0)
+	var posicao_final := Vector3(0, 11.5, 0)
+	var tempo_transicao := EnchenteEstadoDeJogo.TemporizadorGlobal.wait_time - 60
+	tween.interpolate_property($SpriteCidade, "position", posicao_inicial, posicao_final, tempo_transicao, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	tween.start()
