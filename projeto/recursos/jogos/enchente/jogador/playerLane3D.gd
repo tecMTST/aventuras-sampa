551d8b0c2d8d2ede8845c0a25f9af3bce72e288b
ClockWritesDev <herbert@grupozenir.com.br>
2024-09-06 17:42:03 -0300

    adição de baloes de fala

--- projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
+++ projeto/recursos/jogos/enchente/jogador/playerLane3D.gd
@@ -12,6 +12,8 @@ onready var timer_imunidade = $TimerImunidade
 onready var imunidade_modulate = false
 onready var menuOpcoes = preload("res://recursos/jogos/enchente/menu_de_opcoes/MenuDeOpcoes.tscn")
 onready var posicao_sprite_agua_original = sprite_agua.global_position
+onready var BaloesDeFalha: Sprite3D = $BalaoDeFala
+onready var TweenBalao: Tween = $BalaoDeFala/TweenBalao
 
 var imune = false
 var imune_dano = false
@@ -25,6 +27,21 @@ var path_lista_baloes: Array = [
 	"res://elementos/imagem/baloes/moca.png",
 	"res://elementos/imagem/baloes/onibus.png",
 	"res://elementos/imagem/baloes/raiva.png"]
+var lista_de_streamtexture: Array = []
+
+func _ready():
+	for n in range(4):
+		var texturas = StreamTexture.new()
+		var image = Image.new()
+		var image_loader = Image.new()
+
+		if image_loader.load(String(path_lista_baloes[n])):
+			texturas.create_from_image(image)
+			lista_de_streamtexture.append(texturas)
+		else:
+			print("Erro ao carregar a textura: %s" % path_lista_baloes[n])
+	print(lista_de_streamtexture)
+#	_gerar_fala_de_dano()
 
 func _input(event):
 	if Input.is_action_just_pressed("pause"):
@@ -44,9 +61,11 @@ func _on_ControladorArrasta_arrastado(chave):
 func _on_AreaDano_body_entered(body: Node) -> void:
 	if body.is_in_group("terrestre") and not imune and not pulando:
 		vida.receber_dano(1.0)
+		_gerar_fala_de_dano()
 		imunidade(true, tempo_imunidade_dano)
 	if body.is_in_group("aereo") and not imune and not abaixado:
 		vida.receber_dano(1.0)
+		_gerar_fala_de_dano()
 		imunidade(true, tempo_imunidade_dano)
 	if body.is_in_group("invencibilidade"):
 		body.queue_free()
@@ -58,7 +77,7 @@ func _on_AreaDano_body_entered(body: Node) -> void:
 		controle_faixa_3d.pular()
 
 func _on_Vida_vida_acabou() -> void:
-	TrocadorDeCenas.trocar_cena('res://recursos/feed_de_noticias/feed_de_noticia.tscn')
+	TrocadorDeCenas.trocar_cena('res://recursos/Menu_principal/Menu_Principal.tscn')
 
 func imunidade(dano: bool, tempo: float):
 	if imune:
@@ -129,3 +148,26 @@ func _on_SpriteAgua_animation_finished():
 	if anim_cair:
 		anim_cair = false
 		sprite_agua.play("Idle")
+
+func _gerar_fala_de_dano():
+	var random = RandomNumberGenerator.new()
+	var rndbalao = random.randi_range(0, 3)
+	randomize()
+	print(rndbalao)
+	print(lista_de_streamtexture[rndbalao])
+	BaloesDeFalha.texture = lista_de_streamtexture[rndbalao]
+	_animar_tween_balao("Aparecer")
+	$BalaoDeFala/DeixarInvisivel.start(3)
+	yield($BalaoDeFala/DeixarInvisivel, 'timeout')
+	_animar_tween_balao("Desaparecer")
+
+func _animar_tween_balao(anim):
+	if anim == "Aparecer":
+		BaloesDeFalha.visible = true
+		TweenBalao.interpolate_property(BaloesDeFalha, "scale", Vector3(0.54, 0.54, 0.54), Vector3(0.71, 0.71, 0.71), 0.5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+		TweenBalao.start()
+	elif anim == "Desaparecer":
+		TweenBalao.interpolate_property(BaloesDeFalha, "scale", Vector3(0.71, 0.71, 0.71), Vector3(0.54, 0.54, 0.54), 0.5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+		TweenBalao.start()
+		yield(TweenBalao, "tween_completed")
+		BaloesDeFalha.visible = false
