74b43b607e9a4102bff5efcdf15fc7f69f192c2a
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2024-12-19 19:09:43 -0300

    Ajustes - Problema no loop da 2a música - Reduzir movimento da câmera ao acompanhar personagem

--- projeto/recursos/jogos/enchente/cenas/AudioStreamBGM.gd
+++ projeto/recursos/jogos/enchente/cenas/AudioStreamBGM.gd
@@ -3,15 +3,18 @@ extends AudioStreamPlayer
 const bgm = {
 	'1': {
 		'audio': preload("res://elementos/audio/bgm/jogo/lv1-surf.mp3"),
-		'espera': 2.0
+		'espera': 2.0,
+		'loop': false
 	},
 	'2': {
 		'audio': preload("res://elementos/audio/bgm/jogo/lv2-surf.mp3"),
-		'espera': 2.0
+		'espera': 2.0,
+		'loop': true
 	},
 	'4': {
 		'audio': preload("res://elementos/audio/bgm/jogo/lv3-chefe.mp3"),
-		'espera': 2.0
+		'espera': 2.0,
+		'loop': false
 	},
 }
 
@@ -30,4 +33,5 @@ func mudar_musica(fase: String) -> void:
 	tween.reset(self, "volume_db")
 
 	stream = musica.audio
+	stream.loop = musica.loop
 	play()
--- projeto/recursos/jogos/enchente/cenas/CameraEnchente.gd
+++ projeto/recursos/jogos/enchente/cenas/CameraEnchente.gd
@@ -4,6 +4,11 @@ onready var tween := $Tween as Tween
 
 func mover(_direcao, alvo) -> void:
 	var alvo_camera = self.global_transform.origin
-	alvo_camera.x = alvo.x
+	alvo_camera.x = 0.0
+	if alvo.x > 0:
+		alvo_camera.x = 0.5
+	elif alvo.x < 0:
+		alvo_camera.x = -0.5
+
 	tween.interpolate_property(self, "global_transform", global_transform, Transform(global_transform.basis, alvo_camera), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	tween.start()
